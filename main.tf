189b378b09e6fe3bd581cc679ccdacffef8626ef
Arun Visvajeet RS <arur@equinor.com>
2025-11-19 15:20:28 +0100

    Add Branch protection

--- main.tf
+++ main.tf
@@ -55,3 +55,27 @@ resource "github_branch_protection" "bp" {
   required_linear_history = true
   require_conversation_resolution = true
 }
+import {
+  to = github_branch_protection.bp
+  id = "tictactoe:main"
+}
+
+resource "github_branch_protection" "bp" {
+  repository_id = github_repository.tictactoe.node_id
+  # also accepts repository name
+  # repository_id  = github_repository.example.name
+
+  pattern                 = "main"
+  enforce_admins          = true
+  allows_deletions        = false
+  required_linear_history = true
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+
+  }
+  require_conversation_resolution = true
+  allows_force_pushes             = false
+
+}
+
